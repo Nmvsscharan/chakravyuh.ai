654d14efb6fd4fcf13594abba5be4b05ee2a76de
arindm007 <96571928+arindm007@users.noreply.github.com>
2024-04-24 02:47:58 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "chakravyuh.ai",
-      script: "./node_modules/next/dist/bin/next",
-      args: "start -p " + (process.env.PORT || 3000),
+      name: "chakravyuh",
+      script: "yarn",
+      args: "dev",
       watch: false,
       autorestart: true,
     },
